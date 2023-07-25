1cb11ed578c47cd71aa200885453f08d03434247
Federico Tomassini <fedetomassini2@gmail.com>
2023-07-25 12:46:18 -0300

    @font-face

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
-import Swal from 'sweetalert2';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faMessage, faKey, faTrashCan, faFingerprint } from '@fortawesome/free-solid-svg-icons';
+import { faMessage, faFingerprint } from '@fortawesome/free-solid-svg-icons';
 
 import ModuleStyles from '../src/Assets/Styles/App.module.scss';
 import website_logo  from '../src/Assets/Images/website.png';
